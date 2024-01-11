895e19d55dd0a3f313978c99c7c821050f4ebc2f
Raul Santos Andrade <raul.sandrade@hotmail.com>
2024-01-11 20:48:40 -0300

    Resolução primeiro desafio

--- 3411-praticando-logica-projeto_inicial/alugames/js/app.js
+++ 3411-praticando-logica-projeto_inicial/alugames/js/app.js
@@ -1,12 +1,20 @@
-function alterarStatus(status) {
-    var botao = document.querySelector('.dashboard__item__button');
-    var texto_botao = 'Devolver'
-
-    if (status === 1) {
-        botao.textContent = texto_botao
-        botao.style.backgroundColor = '#95a5a6'; // Altera a cor diretamente usando style
+function alterarStatus(id) {
+    let gameClicado = document.getElementById(`game-${id}`);
+    let imagem = gameClicado.querySelector('.dashboard__item__img');
+    let botao = gameClicado.querySelector('.dashboard__item__button');
+   
+    
+    if (imagem.classList.contains('dashboard__item__img--rented')){
+       imagem.classList.remove('dashboard__item__img--rented');
+       botao.classList.remove('dashboard__item__button--return');
+       botao.textContent = 'Alugar';
+       
+    } else{
+        imagem.classList.add('dashboard__item__img--rented');
+        botao.textContent = 'Devolver';
+        botao.classList.add('dashboard__item__button--return')
     }
 
     
-    
+
 }
